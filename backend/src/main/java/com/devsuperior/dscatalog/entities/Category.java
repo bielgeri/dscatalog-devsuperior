f838a256e16427e815578467244135850b3d9ff4
bielgeri <geribolarodrigues@gmail.com>
2026-07-09 19:20:41 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -9,7 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 @Entity
-@Table(name="tb-category")
+@Table(name="tb_category")
 public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
