db44deb48e2dfddebe263510ffc3e2f4df8ba0e8
bielgeri <geribolarodrigues@gmail.com>
2026-07-31 10:45:52 -0300

    Configuration

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -62,7 +62,7 @@ public class CategoryService {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException("ID not found.");
+			throw new ResourceNotFoundException("ID not found teste.");
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity Violation");
 		}
