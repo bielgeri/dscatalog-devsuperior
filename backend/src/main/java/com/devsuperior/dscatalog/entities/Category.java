cf06e7e62f4f4a1a6504e1860a065b72b9813d25
bielgeri <geribolarodrigues@gmail.com>
2026-08-10 20:03:27 -0300

    Different Type of Mock

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -46,7 +46,6 @@ public class Category implements Serializable {
 	}
 
 	public Category(Long id, String name) {
-		super();
 		this.id = id;
 		this.name = name;
 	}
--- backend/src/test/java/com/devsuperior/dscatalog/service/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/service/ProductServiceTests.java
@@ -1,18 +1,27 @@
 package com.devsuperior.dscatalog.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.ProductService;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -26,18 +35,31 @@ public class ProductServiceTests {
 	private long existingId;
 	private long nonExistingId;
 	private long dependentId;
+	private PageImpl<Product> page;
+	private Product product;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		existingId = 20L;
-		nonExistingId = 900L;
+		existingId = 1L;
+		nonExistingId = 2L;
+		dependentId = 3L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
+		
+		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+		
+		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+
 		
 		Mockito.when(repository.existsById(existingId)).thenReturn(true);
 		Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
 		
 		Mockito.doNothing().when(repository).deleteById(existingId);	
-		
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 	}
 	
 	@Test
