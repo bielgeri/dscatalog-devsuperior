8c40304f88971c30b2984190066b30b33e90eb73
bielgeri <geribolarodrigues@gmail.com>
2026-08-31 11:25:52 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
@@ -1,5 +1,120 @@
 package com.devsuperior.dscatalog.config;
 
-public class AuthorizationServerConfig {
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.UUID;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+@Configuration
+public class AuthorizationServerConfig {
+	@Bean
+	@Order(1)
+	SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+		OAuth2AuthorizationServerConfigurer authorizationServerConfigure = OAuth2AuthorizationServerConfigurer.authorizationServer();
+		
+		http.securityMatcher(authorizationServerConfigure.getEndpointsMatcher()).with(
+				authorizationServerConfigure,
+				Customizer.withDefaults())
+		.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+		.exceptionHandling(exceptions -> exceptions.defaultAuthenticationEntryPointFor(
+				new LoginUrlAuthenticationEntryPoint("/login"),
+				new MediaTypeRequestMatcher(MediaType.TEXT_HTML)));
+		
+		return http.build();
+	}
+	
+	
+	@Bean
+	RegisteredClientRepository registeredClientRepository(BCryptPasswordEncoder passwordEncoder) {
+		RegisteredClient client = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("dscatalog")
+				.clientSecret(passwordEncoder.encode("dscatalog123"))
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.redirectUri("https://oauth.pstmn.io/v1/browser-callback")
+				.scope("read")
+				.scope("write")
+				.clientSettings(ClientSettings.builder()
+						.requireProofKey(true)
+						.requireAuthorizationConsent(false)
+						.build())
+				.tokenSettings(TokenSettings.builder()
+						.accessTokenTimeToLive(Duration.ofHours(24))
+						.build()).build();
+		return new InMemoryRegisteredClientRepository(client);
+	}
+	
+	@Bean
+	JWKSource<SecurityContext> jwkSource() {
+		
+		KeyPair keyPair = generateRsaKey();
+		
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		
+		RSAKey rsaKey = 
+				new RSAKey.Builder(publicKey)
+				.privateKey(privateKey)
+				.keyID(UUID.randomUUID().toString())
+				.build();
+		
+		JWKSet  jwkSet  = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
+		
+	}
+
+
+	private KeyPair generateRsaKey() {
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			return keyPairGenerator.generateKeyPair();
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
+	}
+	
+	@Bean 
+	JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+		  return OAuth2AuthorizationServerConfiguration
+	                .jwtDecoder(jwkSource);	
+		  }
+	
+	@Bean
+	AuthorizationServerSettings authorizationServerSettings() {
+		return AuthorizationServerSettings.builder().build();
+	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
@@ -2,7 +2,9 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,12 +14,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean 
+    @Order(2)
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
+                .anyRequest().authenticated()
+            ).formLogin(Customizer.withDefaults())
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
