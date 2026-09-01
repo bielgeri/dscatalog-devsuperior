bd61a2948103a7a90b10f9aafca855f2a12f0249
bielgeri <geribolarodrigues@gmail.com>
2026-09-01 11:01:30 -0300

    Resource Server

--- backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
@@ -3,26 +3,41 @@ package com.devsuperior.dscatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+	
+	private static final String[] OPERATOR_OR_ADMIN = {"products/**", "/categories/**"};
+	private static final String[] ADMIN = {"/users/**"};
 
     @Bean 
     @Order(2)
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().authenticated()
-            ).formLogin(Customizer.withDefaults())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .requestMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
+                .requestMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+                .requestMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .formLogin(Customizer.withDefaults())
+            .oauth2ResourceServer(oauth2 ->
+            oauth2.jwt(jwt ->
+            jwt.jwtAuthenticationConverter(
+                jwtAuthenticationConverter
+            )
+        )
+    );
 
         return http.build();
     }
@@ -30,5 +45,19 @@ public class WebSecurityConfig {
     @Bean AuthenticationManager authenticationManager(
             AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+    
+    @Bean
+    JwtAuthenticationConverter  jwtAuthenticationConverter() {
+    	JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    	
+    	authoritiesConverter.setAuthoritiesClaimName("roles");
+    	authoritiesConverter.setAuthorityPrefix("");
+    	
+    	JwtAuthenticationConverter jwtAutheticationConverter = new JwtAuthenticationConverter();
+    	
+    	jwtAutheticationConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+    	
+    	return jwtAutheticationConverter;
     }
 }
