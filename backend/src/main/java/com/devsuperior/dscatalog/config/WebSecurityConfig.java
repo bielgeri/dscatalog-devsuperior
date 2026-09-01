858fe0a001bb738b2bc4b2963c5b93d5588111f6
bielgeri <geribolarodrigues@gmail.com>
2026-09-01 11:23:01 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Profiles;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
@@ -17,19 +19,34 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 	
+	private final Environment env;
+	
 	private static final String[] OPERATOR_OR_ADMIN = {"products/**", "/categories/**"};
 	private static final String[] ADMIN = {"/users/**"};
+
+    WebSecurityConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean 
     @Order(2)
     SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
+    	
+    	
+    	
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> {
+                if (env.acceptsProfiles(Profiles.of("test"))) {
+                    auth.requestMatchers("/h2-console/**").permitAll();
+                } 
+                
+                auth
                 .requestMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
                 .requestMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
                 .requestMatchers(ADMIN).hasRole("ADMIN")
-                .anyRequest().authenticated())
+                .anyRequest().authenticated();
+            })
+            
             .formLogin(Customizer.withDefaults())
             .oauth2ResourceServer(oauth2 ->
             oauth2.jwt(jwt ->
@@ -38,9 +55,15 @@ public class WebSecurityConfig {
             )
         )
     );
-
+        
+        if (env.acceptsProfiles(Profiles.of("test"))) {
+            http.headers(headers ->
+                headers.frameOptions(frameOptions ->
+                    frameOptions.disable()));
+        }
         return http.build();
     }
+           
     
     @Bean AuthenticationManager authenticationManager(
             AuthenticationConfiguration authConfig) throws Exception {
